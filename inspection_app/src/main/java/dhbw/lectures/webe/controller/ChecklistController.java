1e1cb7bb7351bc399988da4c7a20a6a39543d221
Jens <StudentTIFJ@localhost.de>
2026-03-03 21:59:34 +0100

    Init Start

---
+++ inspection_app/src/main/java/dhbw/lectures/webe/controller/ChecklistController.java
@@ -0,0 +1,17 @@
+package dhbw.lectures.webe.controller;
+
+import dhbw.lectures.webe.domain.Checklist;
+import dhbw.lectures.webe.domain.ChecklistStep;
+import dhbw.lectures.webe.repository.ChecklistRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/checklists")
+public class ChecklistController {
+
+
+}
---
+++ inspection_app/src/main/java/dhbw/lectures/webe/controller/InspectionController.java
@@ -0,0 +1,17 @@
+package dhbw.lectures.webe.controller;
+
+import dhbw.lectures.webe.domain.Inspection;
+import dhbw.lectures.webe.domain.InspectionStep;
+import dhbw.lectures.webe.repository.InspectionRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/inspections")
+public class InspectionController {
+
+
+}
